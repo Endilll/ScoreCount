2bda0565ae916a41f3f9b8faacf5e51671eaa3dd
Endil <brainvlad@gmail.com>
2016-07-15 17:39:11 +0300

    Add visio diagram, relayCommand and 'exit' method to ViewModel

--- ScoreCount/ScoreCount-client/ViewModels/MainViewModel.cs
+++ ScoreCount/ScoreCount-client/ViewModels/MainViewModel.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using oop_project_mvvm;
 
 namespace ScoreCount_client.ViewModels {
-	class MainViewModel {
+	public class MainViewModel {
+		public ICommand ExitCommand { get; set; }
+
+		public MainViewModel() {
+			ExitCommand = new RelayCommand(arg => Exit());
+		}
+
+		void Exit() {
+			Application.Current.Shutdown();
+		}
 	}
 }
